f031cc851d121eed2066f0fcd3c604f12862d5bf
D_O)/ <110585066+taeu98@users.noreply.github.com>
2023-01-10 14:20:09 +0900

    .

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,31 +1,19 @@
-/* 09단계 [ 2차원 배열 ] */
-// 제목 : 행렬 덧셈
-// 문제 번호 : 2738
-
 #include <iostream>
 using namespace std;
 int main()
 {
-    int x, n1, n2;
-    cin >> n1 >> n2;
-    int arr[n1][n2];
-
-    for (int i1 = 0; i1 < n1; i1++)
-        for (int i2 = 0; i2 < n2; i2++)
-            arr[i1][i2] = 0;
-
-    for (int i = 0; i < 2; i++)
-        for (int i1 = 0; i1 < n1; i1++)
-            for (int i2 = 0; i2 < n2; i2++)
+    int D, M[3] = {0, 0, 0};
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+        {
+            cin >> D;
+            if (D >= M[0])
             {
-                cin >> x;
-                arr[i1][i2] += x;
+                M[0] = D;
+                M[1] = i + 1;
+                M[2] = j + 1;
             }
-
-    for (int i1 = 0; i1 < n1; i1++)
-    {
-        for (int i2 = 0; i2 < n2; i2++)
-            cout << arr[i1][i2] << " ";
-        cout << endl;
-    }
+        }
+    cout << M[0] << "\n"
+         << M[1] << ' ' << M[2];
 }
