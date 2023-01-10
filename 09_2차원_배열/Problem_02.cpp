cbff52436642742156de655c21cf396066b40040
D_O)/ <110585066+taeu98@users.noreply.github.com>
2023-01-10 20:56:13 +0900

    .

--- 09_2차원_배열/Problem_02.cpp
+++ 09_2차원_배열/Problem_02.cpp
@@ -1,3 +1,23 @@
 /* 09단계 [ 2차원 배열 ] */
 // 제목 : 행렬 덧셈
 // 문제 번호 : 2738
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int D, M[3] = {0, 0, 0};
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+        {
+            cin >> D;
+            if (D >= M[0])
+            {
+                M[0] = D;
+                M[1] = i + 1;
+                M[2] = j + 1;
+            }
+        }
+    cout << M[0] << "\n"
+         << M[1] << ' ' << M[2];
+}
--- TEST/main.cpp
+++ TEST/main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    bool arr[100][100] = {false};
+    int N, x, y, cnt = 0;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> x >> y;
+        for (int j = x; j < 10 + x; j++)
+            for (int k = y; k < 10 + y; k++)
+                arr[j][k] = true;
+    }
+    for (int i = 0; i < 100; i++)
+        for (int j = 0; j < 100; j++)
+            if (arr[i][j])
+                cnt++;
+    cout << cnt << "\n";
 }
