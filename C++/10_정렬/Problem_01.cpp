81359e6ca5dd4c6bd1a675ed0e0f9f6d47f79743
D_O)/ <taeu1998@gmail.com>
2023-01-11 17:21:05 -0800

    .

--- C++/10_정렬/Problem_01.cpp
+++ C++/10_정렬/Problem_01.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
-
-int main(int arg, char ** arg ){
-    return 0;
+int main()
+{
+    int N, cnt = 0;
+    cin >> N;
+    int arr[N + 1];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[0];
+        if (cnt++ == 0)
+            arr[1] = arr[0];
+        else
+            for (int j = cnt; j > 0; j--)
+            {
+                if (arr[0] < arr[j - 1])
+                    arr[j] = arr[j - 1];
+                else
+                {
+                    arr[j] = arr[0];
+                    break;
+                }
+            }
+    }
+    for (int i = 1; i <= cnt; i++)
+        cout << arr[i] << "\n";
 }
